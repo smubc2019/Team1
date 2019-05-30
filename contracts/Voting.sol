c6706117436af3be22c26b4f0513969e6994c7da
Darryl Ng <ngziliang95@gmail.com>
2019-05-30 13:18:10 +0800

    Added function to calculate expiry date (based on po jid function) and voting %

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -5,13 +5,14 @@ pragma solidity ^0.5.0;
 /// Smart contract project : Voting System
 /// By Team 1: Darryl, Po Jid, Yong Xian
 /// All Rights Reserved, 27 May 2019
+//Date are stored in the Unix epoch format (timestamp)
+//which consists in an integer of the number of seconds since 1970-01-01.
 /*
 1) Able to Register Voters (Those that are voting for the proposal)
 2) Create a new proposals
 3) Calculate outcome of proposal base on 2 decimal points
-TO DO:
-1) Calculate Expiry data based on TTL NOTE: Maybe do not need usedByDate since proposal expired based on TTL
-2) Check expiry
+4) Calculate Expiry data based on TTL NOTE: Maybe do not need usedByDate since proposal expired based on TTL
+1 hour = 36000 seconds
 */
 
 contract Voting {
@@ -43,12 +44,12 @@ contract Voting {
 
 
     struct proposal {
-        uint256 totalVotes; 	        // total available votes (shares) = sum(shares) of all voters
-        uint256 quorum;  	            // minimum quorum (in %) of YES votes needed to pass resolution
-        uint256 yesVotes;		        // Total number of YES votes
-        uint256 noVotes;		        // Total number of NO votes
-        State state;	            // Current state of voting
-        Outcome  outcome;	        // Final resulution : PASSED or DEFEATED
+        uint256 totalVotes;             // total available votes (shares) = sum(shares) of all voters
+        uint256 quorum;                 // minimum quorum (in %) of YES votes needed to pass resolution
+        uint256 yesVotes;               // Total number of YES votes
+        uint256 noVotes;                // Total number of NO votes
+        State state;                // Current state of voting
+        Outcome  outcome;           // Final resulution : PASSED or DEFEATED
         string name;                // Nme of Proposal
         string description;         // Brief description of this proposal
         address owner;
@@ -84,27 +85,28 @@ contract Voting {
             string memory  description,  // , like "Proposal to build helicopter land pad"
             uint8   quorum,             // minimum supporting vote to pass resolution
             //uint8   totalVotes,         // sum of legal votes of all voters, like 100 people total 160 votes as some voters has more votes
-            uint8  TTL,                 // Time to live in days, from beginVoting()
-            uint256 usedByDate
-	) public payable returns(string memory) {
-	    ///values must be set when beginVoting() is called
-	    require(msg.value > 0.01 ether, "at least 0.01 ETH is needed to create a new propsoal");
-	    //new proposal object
-	    proposal memory newProposal = proposal (
-        0, 	        // total available votes (shares) = sum(shares) of all voters
-        quorum,  	            // minimum quorum (in %) of YES votes needed to pass resolution
-        0,		        // Total number of YES votes
-        0,		        // Total number of NO votes
-        State.WAIT,	            // Current state of voting
-        Outcome.PENDING,	        // Final resulution : PASSED or DEFEATED
+            uint256  TTL                 // Time to live in days, from beginVoting()
+            //uint256 usedByDate
+    ) public payable returns(string memory) {
+        ///values must be set when beginVoting() is called
+        require(msg.value > 0.01 ether, "at least 0.01 ETH is needed to create a new propsoal");
+        //new proposal object
+        proposal memory newProposal = proposal (
+        0,          // total available votes (shares) = sum(shares) of all voters
+        quorum,                 // minimum quorum (in %) of YES votes needed to pass resolution
+        0,              // Total number of YES votes
+        0,              // Total number of NO votes
+        State.WAIT,             // Current state of voting
+        Outcome.PENDING,            // Final resulution : PASSED or DEFEATED
         name,                // Nme of Proposal
         description,         // Brief description of this proposal
         msg.sender,
         TTL,                  // Time-to-live in days, set in newProposal()
-        usedByDate
-	   );
-	    uint256 newProposalId = proposalId++;
+        block.timestamp+TTL
+       );
+        uint256 newProposalId = proposalId++;
         proposals[newProposalId] = newProposal;
+
         return name;
     }
     //Calculate total votes holds by all the voters
@@ -117,31 +119,35 @@ contract Voting {
         proposals[pId].totalVotes= totalvotes;
     }
 
+    uint256 public uncastedVotes=0;
     //Cast vote based on id of user starting from 0. Each id tag to a voter address
     function castVoteForProposal(uint8 vote,uint256 userId ,uint256 pId)public{
         //require(vote > 0 ,"vote cannot be equal to 0 or less!");
+      require(checkExpiry(pId)!= true,"Proposal Expired cannot vote!");
       require(voters[userId].voted!=true, "user voted already!" );
-      require(voters[userId].shares>= vote,"user casted votes more than what he owns!");
+      require(voters[userId].shares>= vote && vote<= voters[userId].shares,"user casted votes more than what he owns!");
       if(!voters[userId].voted){
         voters[userId].voted = true;
         if(vote > 0){
             proposals[pId].yesVotes+=vote;
-        }else{
-            proposals[pId].noVotes++;
-
-            }
+        }
         }
     }
+
 
     //CALCULATE THE OUTCOME OF THE VOTING
     function getOutcome(uint256 pId) public {
        // require(proposalToVote.state==State.COMPLETED, "Outcome meaningless");
-       // require(checkExpiry)
+       //require(checkExpiry(pId)!=true,"Proposal Expired cannot get outcome!");
         uint256 quorum = (100 / proposals[pId].quorum) *100;// if 80% = 125 (1.25)
         //qurom is in percentage
         uint256 totalVotes = proposals[pId].totalVotes;
         uint256 requiredVotes = (totalVotes / quorum)* 100; //if total votes is 16, 80% is 12.8
         uint castedVotes = proposals[pId].yesVotes * 100;
+
+        //calculate uncasted votes
+        proposals[pId].noVotes = proposals[pId].totalVotes - proposals[pId].yesVotes;
+
         if(castedVotes > requiredVotes){
             proposals[pId].outcome= Outcome.PASSED;
 
@@ -151,13 +157,16 @@ contract Voting {
           proposals[pId].state= State.COMPLETED;
     }
 
-        // Check used-by-date, set voting state accordingly
-  /*  function checkExpiry() internal {
-        if (now > proposalToVote.usedByDate){
-            proposalToVote.state=State.EXPIRED;
-            VotingIsOver();
+    // Check used-by-date, set voting state accordingly
+    function checkExpiry(uint256 pId) public returns (bool) {
+        bool expired = false;
+        if (now > proposals[pId].usedByDate){
+            expired = true;
+            proposals[pId].state=State.EXPIRED;
+            //VotingIsOver();
         }
-    }*/
+        return expired;
+    }
 
     // abort voting : Administrator call  abort, or perhaps we reached used-by-date and nobody ever voted
     // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -166,29 +175,5 @@ contract Voting {
         proposals[pId].state=State.ABORTED;
         proposals[pId].outcome=Outcome.PENDING;
     }
-
-
-    // voting period expired : calculate the outcome
-    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-   /* function VotingIsOver() public {
-        if (proposalToVote.yesVotes==0 && proposalToVote.noVotes==0){
-            // nobody voted, so considered aborted
-            proposalToVote.state = State.ABORTED;
-            proposalToVote.outcome = Outcome.DEFEATED;
-        }
-        else if (proposalToVote.yesVotes/proposalToVote.totalVotes*100>proposalToVote.quorum){
-                  // We have positive outcome, resolution is passed
-                  // quorum is in %
-                  proposalToVote.state = State.COMPLETED;
-                  proposalToVote.outcome = Outcome.PASSED;
-        }
-        else {
-            // negative outcome, proposal defeated
-            proposalToVote.state = State.COMPLETED;
-            proposalToVote.outcome = Outcome.DEFEATED;
-        }
-    }*/
-
-
 
     }
