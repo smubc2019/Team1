b2051450023d8378adb28549f113c86ca98e4b13
Darryl Ng <ngziliang95@gmail.com>
2019-05-30 12:31:33 +0800

    Added new features and to do list for voting.sol

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -5,7 +5,14 @@ pragma solidity ^0.5.0;
 /// Smart contract project : Voting System
 /// By Team 1: Darryl, Po Jid, Yong Xian
 /// All Rights Reserved, 27 May 2019
-// able to register voters and create new proposal will add in more features tomorrow
+/*
+1) Able to Register Voters (Those that are voting for the proposal)
+2) Create a new proposals
+3) Calculate outcome of proposal base on 2 decimal points
+TO DO:
+1) Calculate Expiry data based on TTL NOTE: Maybe do not need usedByDate since proposal expired based on TTL
+2) Check expiry
+*/
 
 contract Voting {
 
@@ -23,8 +30,6 @@ contract Voting {
         PASSED,     // we reached decision, proposal accepted and passed
         DEFEATED    // we reached decision, proposal is rejected / defeated
     }
-
-
 
     // Table of all registered voters, voter must call registerMe()
     struct voter {
@@ -54,7 +59,7 @@ contract Voting {
      mapping(uint256 => proposal) public proposals;
 
 
-    //registeredVoters
+    //register Voters. Must be done before creation of proposals
     function registerVoters(uint8 shares) public payable returns (address){
         require(shares >0);
         require(msg.value > 0.01 ether, "at least 0.01 ETH is needed to registered a voter");
@@ -84,11 +89,10 @@ contract Voting {
 	) public payable returns(string memory) {
 	    ///values must be set when beginVoting() is called
 	    require(msg.value > 0.01 ether, "at least 0.01 ETH is needed to create a new propsoal");
-
 	    //new proposal object
 	    proposal memory newProposal = proposal (
-        calculateTotalVotes(), 	        // total available votes (shares) = sum(shares) of all voters
-        0,  	            // minimum quorum (in %) of YES votes needed to pass resolution
+        0, 	        // total available votes (shares) = sum(shares) of all voters
+        quorum,  	            // minimum quorum (in %) of YES votes needed to pass resolution
         0,		        // Total number of YES votes
         0,		        // Total number of NO votes
         State.WAIT,	            // Current state of voting
@@ -103,29 +107,49 @@ contract Voting {
         proposals[newProposalId] = newProposal;
         return name;
     }
-
-    function calculateTotalVotes() public returns(uint256){
+    //Calculate total votes holds by all the voters
+    function calculateTotalVotes(uint256 pId) public  {
         uint totalvotes=0;
         for (uint8 i = 0 ; i< numVoters; i++){
 
             totalvotes+=voters[i].shares;
         }
-        return totalvotes;
+        proposals[pId].totalVotes= totalvotes;
     }
 
-    // Given voter's address, return number of votes (share) he/she has
-    /*function getShares(address voter) internal view returns(uint256) {
-        uint256 k=0;
+    //Cast vote based on id of user starting from 0. Each id tag to a voter address
+    function castVoteForProposal(uint8 vote,uint256 userId ,uint256 pId)public{
+        //require(vote > 0 ,"vote cannot be equal to 0 or less!");
+      require(voters[userId].voted!=true, "user voted already!" );
+      require(voters[userId].shares>= vote,"user casted votes more than what he owns!");
+      if(!voters[userId].voted){
+        voters[userId].voted = true;
+        if(vote > 0){
+            proposals[pId].yesVotes+=vote;
+        }else{
+            proposals[pId].noVotes++;
 
-        while (k<proposalToVote.registeredVoters){
-            if (!votersRegister[k].voted && votersRegister[k].voter==voter) {
-                return votersRegister[k].shares;
             }
-            k++;
         }
+    }
 
-        return 0;
-    } */
+    //CALCULATE THE OUTCOME OF THE VOTING
+    function getOutcome(uint256 pId) public {
+       // require(proposalToVote.state==State.COMPLETED, "Outcome meaningless");
+       // require(checkExpiry)
+        uint256 quorum = (100 / proposals[pId].quorum) *100;// if 80% = 125 (1.25)
+        //qurom is in percentage
+        uint256 totalVotes = proposals[pId].totalVotes;
+        uint256 requiredVotes = (totalVotes / quorum)* 100; //if total votes is 16, 80% is 12.8
+        uint castedVotes = proposals[pId].yesVotes * 100;
+        if(castedVotes > requiredVotes){
+            proposals[pId].outcome= Outcome.PASSED;
+
+        }else{
+            proposals[pId].outcome = Outcome.DEFEATED;
+        }
+          proposals[pId].state= State.COMPLETED;
+    }
 
         // Check used-by-date, set voting state accordingly
   /*  function checkExpiry() internal {
@@ -133,118 +157,20 @@ contract Voting {
             proposalToVote.state=State.EXPIRED;
             VotingIsOver();
         }
-    }
-    // return result of voting in a string
-    function GetOutcome() internal view returns(bytes32) {
-        require(proposalToVote.state==State.COMPLETED, "Outcome meaningless");
-
-        bytes32 result="";
-
-        if (proposalToVote.outcome==Outcome.PASSED){
-                result="Proposal ACCEPTED";
-        }
-        else if (proposalToVote.outcome==Outcome.DEFEATED){
-                result="Proposal REJECTED";
-        }
-        else {
-            result="UNKNOWN";
-        }
-
-        return result;
     }*/
-
-
-    // Process vote, update register, Return false if voter voted before
-   /* function acceptVote(address voter) private returns(bool) {
-        uint256 k=0;
-        bool voted=false;
-        bool allow=false;
-
-        // check used-by-date first
-        checkExpiry();
-
-        // We must be in READY state to accept vote, else fail
-        require(proposalToVote.state==State.READY, "Voting is no longer allowed");
-
-
-        // Check if voter have voted before
-        while (k<proposalToVote.registeredVoters){
-            if (votersRegister[k].voter==voter) {
-                if (votersRegister[k].voted){
-                    // voter found, but had voted before
-                    voted=true;
-                    allow=false;
-                }
-                else {
-                    // voter found, but never voted, so record this vote
-                    votersRegister[k].voted=true;
-                    voted=false;
-                    allow=true;
-                }
-                break;
-            }
-            k++;
-        }*/
-
-
-   /*     if (! voted ){
-            if (k>=proposalToVote.registeredVoters){
-                // No vote record found, but voter not registered
-                allow=false;
-            }
-        }
-
-        return allow;
-    } */
-
-
-
-
-    // voter registration
-    // Eligible voter must register intend to vote
-   /* function registerMe(uint256 shares) public {
-        // voter must have at least 1 share to voter
-        // Its meaningless to vote if 1 voter have shares > quorum
-        // quorum is in %
-        require(shares>0 && shares*100 < proposalToVote.totalVotes*proposalToVote.quorum, "Invalid");
-
-        // voting must already been setup, waiting for registration
-        require(proposalToVote.state == State.WAIT, "Registration disallowed now");
-
-       votersRegister[proposalToVote.registeredVoters].voter=msg.sender; // We store voter's address for matching later
-        votersRegister[proposalToVote.registeredVoters].shares=shares;     // we store number of shares (votes) this voter has
-        votersRegister[proposalToVote.registeredVoters].voted=false;      //
-        proposalToVote.registeredVoters++;
-
-
-    } */
-
-
-
-
-    // start voting process, administrator call beginVoting() to start the voting
-  /*  function beginVoting() public {
-        require(proposalToVote.state == State.WAIT, "Not ready yet"); // must be in WAIT state first
-        require(proposalToVote.registeredVoters>3 && proposalToVote.totalVotes==0);// cant vote if less than 3 voters
-
-        proposalToVote.usedByDate = now + proposalToVote.TTL;
-        proposalToVote.state = State.READY;
-    }
-
 
     // abort voting : Administrator call  abort, or perhaps we reached used-by-date and nobody ever voted
     // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    function abort() public {
-        require(proposalToVote.state != State.COMPLETED, "Cannot abort completed voting"); // can't abort completed voting
-
-        proposalToVote.state=State.ABORTED;
-        proposalToVote.outcome=Outcome.NONE;
+    function abort(uint256 pId) public {
+        require(proposals[pId].state != State.COMPLETED, "Cannot abort completed voting"); // can't abort completed voting
+        proposals[pId].state=State.ABORTED;
+        proposals[pId].outcome=Outcome.PENDING;
     }
 
 
     // voting period expired : calculate the outcome
     // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    function VotingIsOver() public {
+   /* function VotingIsOver() public {
         if (proposalToVote.yesVotes==0 && proposalToVote.noVotes==0){
             // nobody voted, so considered aborted
             proposalToVote.state = State.ABORTED;
@@ -264,43 +190,5 @@ contract Voting {
     }*/
 
 
-    // voter cast his/her vote, a voter can only cast at most 1 vote, however voting is not compulsory
-    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    /*function newVote(
-        bool accept	// True to accept, False to reject
-        ) public returns(bool) {
-
-
-        // voter must be valid and not voted before and have >0 shares
-        require(getShares(msg.sender)>0, "Cannot vote when voter does not have any share");
-
-        // voter must have registered before, and have never voted before
-        require(acceptVote(msg.sender), "Must register to vote, and at most 1 vote is allowed");
-
-
-
-        // Must be in READY state to vote
-        require(proposalToVote.state==State.READY, "Voting not allowed at the moment");
-
-        bool result=false;
-
-        if (accept){// (this) voter voted YES, 1 share = 1 vote
-              proposalToVote.yesVotes+=getShares(msg.sender);
-              result=true;
-        }
-        else {// this voter voted NO
-            proposalToVote.noVotes+=getShares(msg.sender);
-            result=true;
-        }
-
-        if (100*(proposalToVote.yesVotes/proposalToVote.totalVotes)>=proposalToVote.quorum){
-              // Minimum YES votes (qourum) achieved //
-              proposalToVote.outcome = Outcome.PASSED;
-
-        }
-        else {
-             proposalToVote.outcome = Outcome.DEFEATED;
-        }
-      }*/
 
     }
