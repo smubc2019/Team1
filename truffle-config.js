b6bf23dc47c07492a2cc6a93f6f9b8787c54df9c
unknown <yongxian@lam.sg>
2019-06-02 01:28:25 +0800

    Config ropsten testnet

--- truffle-config.js
+++ truffle-config.js
@@ -18,11 +18,11 @@
  *
  */
 
-// const HDWalletProvider = require('truffle-hdwallet-provider');
-// const infuraKey = "fj4jll3k.....";
+const HDWalletProvider = require('truffle-hdwallet-provider');
+//const infuraKey = "fj4jll3k.....";
 //
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+const fs = require('fs');
+const mnemonic = fs.readFileSync("./secret.txt").toString().trim();
 const path = require("path");
 module.exports = {
     contracts_build_directory: path.join(__dirname, "client/src/contracts"),
@@ -37,6 +37,14 @@ module.exports = {
    */
 
   networks: {
+    ropsten: {
+        provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/5a02a4297abc46eea2ea176f294e203e`),
+        network_id: 3,       // Ropsten's id
+        gas: 5500000,        // Ropsten has a lower block limit than mainnet
+        confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+        timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+        skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    },
     // Useful for testing. The `development` name is special - truffle uses it by default
     // if it's defined here and no other network is specified at the command line.
     // You should run a client (like ganache-cli, geth or parity) in a separate terminal
